b9974fca1af5c45221f2d6531076ebd37652c6a3
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2024-11-10 16:31:04 -0300

    created models

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 use dotenv::dotenv;
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres};
 
 mod db {
     pub mod config;
+}
+
+#[derive(Clone)]
+pub struct AppState {
+    postgres_client: Pool<Postgres>,
 }
 
 #[get("/")]
@@ -15,8 +21,13 @@ async fn main() -> std::io::Result<()>{
 
     dotenv().ok();
     let _pool = db::config::start_connection().await;
-    HttpServer::new(|| {
+    HttpServer::new(move || {
         App::new()
+            .app_data(
+                web::Data::new(AppState {
+                    postgres_client: _pool.clone(),
+                })
+            )
             .service(index)
     }).bind(("127.0.0.1", 8080))?.run().await
 }
