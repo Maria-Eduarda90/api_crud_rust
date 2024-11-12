07d1f67efc330b31c42727dda9940bc9411d7917
Maria-Eduarda90 <snowdennaruto90@gmail.com>
2024-11-12 19:57:02 -0300

    criação da rota para criação do usuario

--- src/services/users/services.rs
+++ src/services/users/services.rs
@@ -28,6 +28,46 @@ async fn get_all_users(app_state: web::Data<AppState>) -> impl Responder {
     }
 }
 
+#[post("/users")]
+async fn create_user(app_state: web::Data<AppState>, user: web::Json<RegisterUser>) -> impl Responder {
+    let hasded = hash(&user.password, DEFAULT_COST).expect("Failed to hash password");
+    let email_check = sqlx::query!("SELECT id FROM users WHERE email = $1", user.email)
+        .fetch_optional(&app_state.postgres_client)
+        .await;
+
+    match email_check {
+        Ok(Some(_)) => {
+            return HttpResponse::Conflict().body("Email ja cadastrado");
+        },
+        Ok(None) => {},
+        Err(_) => {
+            return HttpResponse::InternalServerError().body("Erro ao verificar email");
+        }
+    }
+
+    if !(hasded != user.password){
+        return HttpResponse::InternalServerError().body("Error trying to hash password");
+    }
+
+    let result = sqlx::query!(
+        "INSERT INTO users (name, email, password) VALUES ($1, $2, $3) RETURNING id, name, email, password",
+        user.name, 
+        user.email, 
+        hasded
+    ).fetch_one(&app_state.postgres_client).await;
+
+    match result {
+        Ok(user) => HttpResponse::Ok().json(AllUser {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            password: user.password,
+        }),
+        Err(_) => HttpResponse::InternalServerError().body("Error trying to create user.")
+    }
+}
+
 pub fn users_routes(cfg: &mut web::ServiceConfig){
     cfg.service(get_all_users);
+    cfg.service(create_user);
 }
